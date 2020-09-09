5f2251fcbd16ba74f442b960aeed1bdae53b0a23
Arun Karikalan <arun.digital.karikalan1994@gmail.com>
2020-09-09 12:49:46 +0530

    socks_problem

---
+++ sock_pair_problem/solution.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the sockMerchant function below.
+function sockMerchant(n, ar) {
+    let countObj = ar.reduce((countObj, curr) => {
+        countObj[curr] = countObj[curr] ? countObj[curr] + 1 : 1;
+        return countObj
+    }, {})
+    return Object.keys(countObj).reduce((pairs, key) => {
+        return Math.floor(countObj[key] / 2) + pairs
+    }, 0)
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const ar = readLine().split(' ').map(arTemp => parseInt(arTemp, 10));
+
+    let result = sockMerchant(n, ar);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
